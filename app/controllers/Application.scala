598eee203d80d96b8d68dc65c2912666422e2178
Misha Wagner <mishajw@gmail.com>
2015-12-10 13:20:09 +0000

    Timeout addedd

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import java.util.UUID
-import javax.mail.AuthenticationFailedException
 
 import backend.MailSender
 import play.api.data.Forms._
@@ -9,114 +8,133 @@ import play.api.data._
 import play.api.mvc._
 
 import scala.collection.mutable
+import scala.util.Try
 
 object Application extends Controller {
-  val loginStorage: mutable.Map[String, MailSender] = mutable.Map()
 
-  /**
-    * Show the login page
-    */
-  def loginPage = Action {
-    Ok(views.html.loginPage())
-  }
+	case class LoginDetail(email: String, pass: String, server: String, mailSender: Option[MailSender])
 
-  /**
-    * Log in with email details
-    */
-  def login = Action { implicit request => try {
-      val (email, pass, server) = loginForm.bindFromRequest.get
-      val mailSender = new MailSender(email, pass, server)
-      val uuid = UUID.randomUUID().toString
+	val loginStorage: mutable.Map[String, LoginDetail] = mutable.Map()
 
-      loginStorage(uuid) = mailSender
+	/**
+		* Show the login page
+		*/
+	def loginPage = Action {
+		Ok(views.html.loginPage())
+	}
 
-      Redirect(routes.Application.mail()).withSession("sid" -> uuid)
-    } catch {
-      case e: AuthenticationFailedException =>
-        Redirect(routes.Application.loginFailed)
-      case e: Throwable =>
-        Redirect(routes.Application.loginFailed)
-    }
-  }
+	/**
+		* Log in with email details
+		*/
+	def login = Action { implicit request =>
+		val (email, pass, server) = loginForm.bindFromRequest.get
+		val uuid = UUID.randomUUID().toString
 
-  /**
-    * Allow user to send mail
-    */
-  def mail = Action (implicit request => sid match {
-    case Some(sid) => Ok(views.html.sendMail())
-    case None => Redirect(routes.Application.loginPage())
-  })
+		try {
+			val mailSender = new MailSender(email, pass, server)
 
-  /**
-    * Send mail
-    */
-  def send = Action { implicit request =>
-    sid match {
-      case Some(sid) =>
-        val (to, subject, body) = sendForm.bindFromRequest.get
-        val mailSender = loginStorage(sid)
+			loginStorage(uuid) = LoginDetail(email, pass, server, Some(mailSender))
 
-        mailSender.send(to, subject, body)
+			Redirect(routes.Application.mail()).withSession("sid" -> uuid)
+		} catch {
+			case e: Throwable =>
+				loginStorage(uuid) = LoginDetail(email, pass, server, None)
+				Redirect(routes.Application.loginFailed()).withSession("sid" -> uuid)
+		}
+	}
 
-        Redirect(routes.Application.sent())
-      case None =>
-        Redirect(routes.Application.loginPage())
-    }
-  }
+	def retryLogin = Action { implicit request =>
+		sid match {
+			case Some(sid) =>
+				val loginDetail = loginStorage(sid)
+				Ok(views.html.loginPage(loginDetail.email, loginDetail.pass, loginDetail.server))
+			case None =>
+				Ok(views.html.loginPage())
+		}
 
-  /**
-    * Page showing successful send
-    */
-  def sent = Action {
-    Ok(views.html.sent())
-  }
+	}
 
-  /**
-    * Log out the user
-    * @return
-    */
-  def logoutPage() = Action { implicit request =>
-    sid match {
-      case Some(sid) =>
-        loginStorage(sid).close()
-        loginStorage remove sid
-      case None =>
-    }
+	/**
+		* Allow user to send mail
+		*/
+	def mail = Action (implicit request => sid match {
+		case Some(sid) => Ok(views.html.sendMail())
+		case None => Redirect(routes.Application.loginPage())
+	})
 
-    Redirect(routes.Application.loginPage())
-  }
+	/**
+		* Send mail
+		*/
+	def send = Action { implicit request =>
+		sid match {
+			case Some(sid) =>
+				val (to, subject, body) = sendForm.bindFromRequest.get
 
-  /**
-    * Page showing unsuccessful login
-    */
-  def loginFailed = Action {
-    Ok(views.html.loginFailed())
-  }
+				loginStorage(sid).mailSender match {
+					case Some(mailSender) =>
+						mailSender.send(to, subject, body)
+						Redirect(routes.Application.sent())
+					case None =>
+						Redirect(routes.Application.loginPage())
+				}
+			case None =>
+				Redirect(routes.Application.loginPage())
+		}
+	}
 
-  /**
-    * Get the Session ID from the request
-    */
-  def sid(implicit request: Request[AnyContent]) = {
-    request.session.get("sid")
-  }
+	/**
+		* Page showing successful send
+		*/
+	def sent = Action {
+		Ok(views.html.sent())
+	}
 
-  /**
-    * Form for logging in
-    */
-  def loginForm = Form(tuple(
-      "email" -> text,
-      "pass" -> text,
-      "server" -> text
-    )
-  )
+	/**
+		* Log out the user
+		* @return
+		*/
+	def logoutPage() = Action { implicit request =>
+		sid match {
+			case Some(sid) =>
+				Try(loginStorage(sid).mailSender.get.close())
+				loginStorage remove sid
+			case None =>
+		}
 
-  /**
-    * Form for sending emails
-    */
-  def sendForm = Form(tuple(
-      "to" -> text,
-      "subject" -> text,
-      "body" -> text
-    )
-  )
+		Redirect(routes.Application.loginPage())
+	}
+
+	/**
+		* Page showing unsuccessful login
+		*/
+	def loginFailed = Action {
+		Ok(views.html.loginFailed())
+	}
+
+	/**
+		* Get the Session ID from the request
+		*/
+	def sid(implicit request: Request[AnyContent]) = {
+		request.session.get("sid")
+	}
+
+	/**
+		* Form for logging in
+		*/
+	def loginForm = Form(tuple(
+		"email" -> text,
+		"pass" -> text,
+		"server" -> text
+	)
+	)
+
+	/**
+		* Form for sending emails
+		*/
+	def sendForm = Form(tuple(
+		"to" -> text,
+		"subject" -> text,
+		"body" -> text
+	)
+	)
 }
